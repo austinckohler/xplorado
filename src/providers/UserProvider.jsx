31550514a556cf26c3a98773c4dc12ce66ab4a95
austinckohler <austinckohler@gmail.com>
2020-06-29 22:25:40 -0600

    fixed formatting for providers

--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -1,29 +1,31 @@
-import  React, { Component, createContext } from 'react'
-import  { auth, generateUserDocument } from '../firebase'
+import React, { Component, createContext } from "react";
+import { auth, generateUserDocument } from "../firebase";
 
-export const UserContext = createContext({ user: null })
+
+export const UserContext = createContext({ user: null });
 
 export default class UserProvider extends Component {
-    state = {
-        user: null
-    }
+  state = {
+    user: null
+  };
 
 //allows new users to create an account using email and password.
 componentDidMount = async () => {
     auth.onAuthStateChanged(async userAuth => {
-        const user = await generateUserDocument(userAuth)
-        this.setState({ user })
-    })
-};
+      const user = await generateUserDocument(userAuth);
+      this.setState({ user });
+    });
 
 
-render() {
-    const { user } = this.state
+  };
+
+  render() {
+    const { user } = this.state;
 
     return (
-        <UserContext.Provider value={user}>
-            {this.props.children}
-        </UserContext.Provider>
-    )
-}
+      <UserContext.Provider value={user}>
+        {this.props.children}
+      </UserContext.Provider>
+    );
+  }
 }
